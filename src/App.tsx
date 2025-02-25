46fa0c4fa513bfe9accae5c731c184f84c0692fb
Scott Schafer <scott.schafer@gmail.com>
2025-02-24 22:24:33 -0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -45,11 +45,11 @@ function App() {
           ['', 'sm', 'lg'].map(size => (
             <div className="stack-v gap-md w-full items-center" key={`size-${size}`}>
               <label>Buttons {size ? `(${size})` : ''}</label>
-              <div className="stack-h gap-md">
+              <div className="stack-h gap-lg">
                 <button title="Primary Button!" data-tooltip-placement="left" className={`primary ${size}`}>primary</button>
                 <button title="Secondary Button!" data-tooltip-placement="right" className={`secondary ${size}`}>secondary</button>
                 <button className={`text ${size}`}>text</button>
-                <button className={`warning icon-error ${size}`}>warning</button>
+                <button className={`warning icon-warning ${size}`}>warning</button>
               </div>
             </div>
           ))
